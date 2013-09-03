200b6317e4e1a899f5f22b805f0784166bc5cb00
David Baines <david.peter.baines@gmail.com>
2013-09-03 19:26:35 +0800

    Just a test

--- bot.py
+++ bot.py
@@ -1,12 +1,21 @@
 import time
 import praw
-r = prawa.Reddit('PRAW related-question monitor by u/_Daimon_ v 1.0. '
-...                 'Url: https://praw.readthedocs.org/en/latest/'
-...                 'pages/writing_a_bot.html')
-r.login()
-already_done = []
 
-while True:
-	subreddit = r.get_subreddit('learnpython')
-	for submission in subreddit.get_hot(limit=10):
-		
+user_agent = ("Repostbot /u/darkrai9292 "
+               "https://github.com/darkrai9292/Repost-Bot")
+
+r = praw.Reddit(user_agent=user_agent)
+r.login()
+
+user_name = "darkrai9292"
+user = r.get_redditor(user_name)
+
+limit = 20
+gen = user.get_submitted(limit=limit)
+
+karma_by_subreddit = {}
+for thing in gen:
+	subreddit = thing.subreddit.display_name
+	karma_by_subreddit[subreddit] = (karma_by_subreddit.get(subreddit, 0) + thing.score)
+
+print str(karma_by_subreddit)
